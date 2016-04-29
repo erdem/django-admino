7810e75b622fc83002d653bc8ec880ca107f4006
erdem <erdemozkol@gmail.com>
2016-04-29 12:19:53 +0100

    removed dummy meta classses

--- admino/serializer.py
+++ admino/serializer.py
@@ -2,19 +2,3 @@ from collections import OrderedDict
 
 from django.utils import six
 
-
-class SerializerMetaclass(type):
-
-    @classmethod
-    def _get_declared_fields(cls, bases, attrs):
-        pass
-
-    def __new__(cls, name, bases, attrs):
-        attrs["fields"] = cls._get_declared_fields(bases, attrs)
-        return super(SerializerMetaclass, cls).__new__(cls, name, bases, attrs)
-
-
-@six.add_metaclass(SerializerMetaclass)
-class BaseSerializer(object):
-    def __init__(self, admin_instance, *args, **kwargs):
-        pass
